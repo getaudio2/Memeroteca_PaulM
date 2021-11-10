c23c218c13ad64a50654ab15ee0538a31e3c661d
Paul <paugermaisar@hotmail.es>
2021-11-10 19:37:29 +0100

    Added Meme.java class

---
+++ app/src/main/java/com/example/memeroteca_paulm/Meme.java
@@ -0,0 +1,35 @@
+package com.example.memeroteca_paulm;
+
+public class Meme {
+
+    private int id;
+    private String name;
+    private String url;
+    private int width;
+    private int height;
+    private int box_count;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getBox_count() {
+        return box_count;
+    }
+}
